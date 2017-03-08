88f4e2a15c8756b69c1d4a14501b656dd4814026
billyhayes <williamthekid@gmail.com>
2017-03-08 14:32:14 -0700

    test file

---
+++ test.py
@@ -0,0 +1,36 @@
+import json
+import boto3
+import operator
+from datetime import datetime, timedelta, tzinfo
+
+ACCOUNT = '356735905217'
+
+def copy_latest_snapshot():
+    client = boto3.client('rds', 'us-east-1')
+    backup_client = boto3.client('rds', 'us-west-1')
+
+    response = client.describe_db_snapshots(
+        SnapshotType='automated',
+        IncludeShared=False,
+        IncludePublic=False
+    )
+
+    if len(response['DBSnapshots']) == 0:
+        raise Exception("No automated snapshots found")
+
+    snapshots_per_project = {}
+    for snapshot in response['DBSnapshots']:
+        if snapshot['Status'] != 'available':
+            continue
+
+    if snapshot['DBInstanceIdentifier'] not in snapshots_per_project.keys():
+        snapshots_per_project[snapshot['DBInstanceIdentifier']] = {}
+
+    snapshots_per_project[snapshot['DBInstanceIdentifier']][snapshot['DBSnapshotIdentifier']] = snapshot[
+        'SnapshotCreateTime']
+
+
+
+
+
+    ## if datetime.datetime.today().day = 1:
